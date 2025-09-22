dfcf75e5a3e13cffa0d50a831acccd80776f8736
Mykyta Krasiukov <mykytakrasiukovgit@gmail.com>
2025-09-22 18:16:22 +0300

    added first endpoints

--- src/AnimeEncyclopedia.API/Program.cs
+++ src/AnimeEncyclopedia.API/Program.cs
@@ -1,6 +1,6 @@
 using AnimeEncyclopedia.Infrastructure;
 using Microsoft.EntityFrameworkCore;
-
+using AnimeEncyclopedia.Domain;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -9,5 +9,16 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 var app = builder.Build();
 
 app.MapGet("/", () => "Anime Encyclopedia API is running!");
+
+
+app.MapGet("/anime", async (AppDbContext db) =>
+    await db.Animes.Include(a => a.Genre).ToListAsync());
+
+app.MapPost("/anime", async (Anime anime, AppDbContext db) =>
+{
+    db.Animes.Add(anime);
+    await db.SaveChangesAsync();
+    return Results.Created($"/anime/{anime.Id}", anime);
+});
 
 app.Run();
